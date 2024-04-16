15da4407bbcad98e073f53ac96a04d738edb3db3
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-16 07:28:30 -0700

    AirBnb2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,3 @@
-SET character_set_connection = utf8mb4;
-SET character_set_client = utf8mb4;
-SET character_set_results = utf8mb4;
-SET collation_connection = utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
