23a2a2e559256228ff70a3d18a444b0acb2bf24c
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-05-18 22:08:46 +0000

    AirBnb

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+import sys
+
+skills_app = Flask(__name__)
+@skills_app.route("/", strict_slashes=False)
+def hello():
+        return ("Hello HBNB!")
+@skills_app.route("/hbnb", strict_slashes=False)
+def hb():
+        return ("HBNB")
+@skills_app.route("/c/<text>", strict_slashes=False)
+def c_rep(text):
+        text = text.replace('_', ' ')
+        return ("C {}".format(text))
+@skills_app.route("/python", strict_slashes=False)
+def py_def():
+        return ("Python is cool")
+@skills_app.route("/python/", strict_slashes=False)
+def py_defs():
+	return ("Python is cool")
+@skills_app.route("/python/<text>", strict_slashes=False)
+def py_rep(text):
+	text = text.replace('_', ' ')
+	return ("Python {}".format(text))
+if __name__ == "__main__":
+        skills_app.run(debug=True, host='0.0.0.0', port=5000)
