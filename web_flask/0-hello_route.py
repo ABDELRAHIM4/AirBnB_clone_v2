93c7fb1ea6441124b5bdc4c1152c24e5c2c31296
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-06-12 18:52:18 +0000

    devops

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-        return "Hello HBNB!"
+    return "Hello HBNB!"
+
+
 @app.route("/airbnb-onepage/", strict_slashes=False)
 def air():
     return ("Hello HBNB!")
+
+
 if __name__ == "__main__":
-        app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
