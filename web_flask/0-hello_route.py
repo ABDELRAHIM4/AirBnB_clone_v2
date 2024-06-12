cc64ac36401a7b231cf3deaa4f7fc0f2dc1a3fbe
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-06-12 18:44:43 +0000

    devops

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask
-skills_app = Flask(__name__)
-@skills_app.route("/")
+app = Flask(__name__)
+@app.route("/")
 def hello():
         return "Hello HBNB!"
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def air():
+    return ("Hello HBNB!")
 if __name__ == "__main__":
-        skills_app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
